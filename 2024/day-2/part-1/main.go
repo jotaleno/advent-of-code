1b99c0a965f4c83469c78d0190caa352ed934b71
jotaleno <24483665+jotaleno@users.noreply.github.com>
2024-12-02 19:44:08 +0000

    Add day 2 - part 1

---
+++ 2024/day-2/part-1/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const FILE_NAME = "input.txt"
+
+const MIN_DIFF = 1
+const MAX_DIFF = 3
+
+func main() {
+	f, err := os.ReadFile(FILE_NAME)
+
+	if err != nil {
+		fmt.Printf("Could not open file: %s\n", FILE_NAME)
+
+		os.Exit(-1)
+	}
+
+	points := totalScore(string(f))
+
+	fmt.Println(points)
+}
+
+func totalScore(puzzle string) int {
+	totalScore := 0
+
+	lines := strings.Split(strings.TrimSpace(puzzle), "\n")
+
+	for _, line := range lines {
+		levels := strings.Fields(line)
+
+		if isSafe(levels) {
+			totalScore++
+		}
+	}
+
+	return totalScore
+}
+
+func isSafe(report []string) bool {
+	for i := 1; i < len(report)-1; i++ {
+		previousNumber, _ := strconv.Atoi(report[i-1])
+		currentNumber, _ := strconv.Atoi(report[i])
+		nextNumber, _ := strconv.Atoi(report[i+1])
+
+		previousDiff := int(math.Abs(float64(currentNumber - previousNumber)))
+		nextDiff := int(math.Abs(float64(nextNumber - currentNumber)))
+
+		if previousDiff < MIN_DIFF || previousDiff > MAX_DIFF || nextDiff < MIN_DIFF || nextDiff > MAX_DIFF {
+			return false
+		}
+
+		if (currentNumber-previousNumber > 0 && nextNumber-currentNumber < 0) || (currentNumber-previousNumber < 0 && nextNumber-currentNumber > 0) {
+			return false
+		}
+	}
+
+	return true
+}
